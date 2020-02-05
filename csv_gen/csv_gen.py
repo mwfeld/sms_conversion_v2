2ad136f4abaa0e858ad6d57eebc3034f4f1d6b43
Matt Feldmann <mfeldman@fastenal.com>
2020-02-05 16:10:55 -0500

    Add vend pack function and conditional bug fix

--- csv_gen/csv_gen.py
+++ csv_gen/csv_gen.py
@@ -24,7 +24,7 @@ def main():
                 working_list.append('{}|{}'.format(row[2][3:], row[0]))
                 working_list.append('siteId={}'.format(row[4][3:]))
                 working_list.append('productSku={}'.format(row[5]))
-                working_list.append(strip_space_brackets(row[6]))
+                working_list.append(format_bracketed_sku(row[6]))
                 eap_data.append(working_list)
 
     with open(os.path.join(os.getcwd(), 'bracketed_skus.csv'), 'w', newline='') as new_csv:
@@ -34,15 +34,29 @@ def main():
 
 def strip_space_brackets(sku):
     p_sku_space = re.compile(r'(^.*)(\[\d*\]$)')
-    re_result = re.search(p_sku_space, sku)
-    stripped_sku = '{}{}'.format(re_result.group(1).strip(), re_result.group(2))
-    return stripped_sku
+    if re.search(p_sku_space, sku):
+        re_result = re.search(p_sku_space, sku)
+        stripped_sku = '{}{}'.format(re_result.group(1).strip(), re_result.group(2))
+        return stripped_sku
+    else:
+        return sku
 
-def vend_pack_to_upper(sku):
-    pass
+def vendpack_to_upper(sku):
+    p_sku_v = re.compile(r'^.*(v)\d*\[\d*\]$')
+    if re.match(p_sku_v, sku):
+        match_index = re.match(p_sku_v, sku).start(1)
+        sku_upper_v = ''.join([
+            sku[:match_index],
+            sku[match_index].upper(),
+            sku[match_index + 1:]])
+        return sku_upper_v
+    else:
+        return sku
 
 def format_bracketed_sku(sku):
-    pass
+    formatted_sku = strip_space_brackets(sku)
+    formatted_sku = vendpack_to_upper(formatted_sku)
+    return formatted_sku
 
 
 if __name__ == '__main__':
